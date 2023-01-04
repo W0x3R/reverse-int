54a9e48dfb754e235cc64fcf2e17451d94d9931f
Stanislau Vyrski <vyr4376@yandex.ru>
2023-01-04 19:54:42 +0300

    create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n){
+  if (n>'0') {
+  n = n + "";
+  return n.split("").reverse().join("");
+}
+else if (n<'0') {
+   n = n + "";
+  return n.split("").reverse().join("").slice(0, -1);
+}
 }

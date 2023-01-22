a09e670cc3f1154a81724757e59e77e16ef66cb6
Stanislau Virsky <114683300+W0x3R@users.noreply.github.com>
2023-01-22 19:21:48 +0300

    add: new decision

--- src/index.js
+++ src/index.js
@@ -8,3 +8,9 @@ else if (n<'0') {
   return n.split("").reverse().join("").slice(0, -1);
 }
 }
+
+// // Второй способ : 
+// module.exports = function reverse (n){
+//   n =Math.abs(n)
+//     return n.toString().split('').reverse().join('')
+// }
